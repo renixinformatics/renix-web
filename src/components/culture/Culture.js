c9be6a59de84b11a81a98e76d51e0aa5e8675b8e
priya-renix <priya.iyyappan@renixinformatics.com>
2022-06-12 19:14:48 +0530

    changes added

--- src/components/culture/Culture.js
+++ src/components/culture/Culture.js
@@ -6,11 +6,11 @@ function Culture() {
     <div className="culture-section">
       <div className="culture-header">Our culture</div>
       <div className="culture-content">
-        We cultivate the open-minded and real passionate values to our system
-        which<br/> enable our workfoce to provide the best services to our clients.
-        We work on<br/> distributed and flexible fashion to solve business problems.
-        We adapt community<br/> based work culture to collaborate and create the big
-        new solutions for the world.
+        We cultivate open-mindedness and work out of sheer passion. <br/>
+        Our team’s priority is to provide the best services to our clients. <br/>
+        Our talent is spread across the world who work flexibly to solve business problems. <br/>
+        We believe in the power of community and our work culture promotes collaboration and creativity.
+
       </div>
     </div>
   );
--- src/components/history/History.js
+++ src/components/history/History.js
@@ -6,13 +6,12 @@ function History() {
     <div className="history-section">
       <div className="history-header">Our History</div>
       <div className="history-content">
-        Our story began in 2020 post covid where the need for AIaaS (AI as a
-        Service) <br/>was gaining traction in the tech industry. Realizing the
-        importance of growing <br/>demand in AI services and niched problems, we at
-        Renix Informatics started to <br/>form a decentralized team to work on
-        various use cases in the AI domain. <br/>The team, culture, framework and
-        tools that we built are capable of solving <br/>challenging business use
-        cases and help customers to make smart decisions.
+        Our journey began in 2020, when an accelerated need for AIaaS(AI as a service) across industries<br/>
+        was identified. With expertise in this space, we formed a decentralised  team that’s excited to <br/>
+        present solutions to various use cases in the AI domain. Our team, its culture, structural <br/>
+        framework and design tools are the cornerstone of solving challenging business problems  <br/>
+        and help customers make smart decisions. 
+
       </div>
     </div>
   );
